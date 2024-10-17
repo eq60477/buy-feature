16ed0b479f9d51e2b2086babe00a3d7338fbb4d5
Simon Buteau <simon.buteau@bell.ca>
2024-10-17 14:54:14 +0000

    feat: plat4-314 ci-cd remove multiple browser

--- e2e/shopping-cart.spec.ts
+++ e2e/shopping-cart.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Shopping Cart Feature", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:3000");
+    await page.goto("/");
   });
 
   test("should display shopping cart feature and add item to cart", async ({ page }) => {
